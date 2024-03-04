0e55b77ef143282514e174d85fcb5659376eed8e
EdinDe <edindehari@MacBook-Air-tilhrende-Edin.local>
2024-03-04 11:39:07 +0100

    1st1

---
+++ src/Adventure.java
@@ -0,0 +1,47 @@
+public class Adventure {
+
+    private Room currentRoom;
+
+
+    public Adventure() {
+        Room room1 = new Room("Room 1", "");
+        Room room2 = new Room("Room 2","");
+        Room room3 = new Room("Room 3","");
+        Room room4 = new Room("Room 4","");
+        Room room5 = new Room("Room 5","");
+        Room room6 = new Room("Room 6","");
+        Room room7 = new Room("Room 7","");
+        Room room8 = new Room("Room 8","");
+        Room room9 = new Room("Room 9","");
+        currentRoom = room1;
+        room1.setEast(room2);
+        room1.setSouth(room4);
+        room2.setWest(room1);
+        room2.setEast(room3);
+        room3.setWest(room2);
+        room3.setSouth(room6);
+        room4.setNorth(room1);
+        room4.setSouth(room7);
+        room5.setSouth(room8);
+        room6.setNorth(room3);
+        room6.setNorth(room9);
+        room7.setNorth(room4);
+        room7.setEast(room8);
+        room8.setWest(room7);
+        room8.setNorth(room5);
+        room8.setEast(room9);
+        room9.setWest(room8);
+        room9.setNorth(room6);
+    }
+
+    public boolean goNorth(){
+        if (currentRoom.equals(null)){
+            return false;
+        }else{
+            currentRoom = currentRoom.getNorth();
+            return true;
+
+        }
+
+    }
+}
---
+++ src/Room.java
@@ -0,0 +1,47 @@
+public class Room {
+
+
+    private String name;
+    private String description;
+    private Room north;
+    private Room east;
+    private Room west;
+    private Room south;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+}
